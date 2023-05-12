f77250f11df8e39c10f15f4c2afa31a61c38b49a
vrha100 <vrha100@gmail.com>
2023-05-12 08:35:37 -0500

    Mi primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ejemplo de node# node_2559206
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs = require('hbs')
+
+const puerto = 8087
+
+//Servidor de contenido estatico
+app.use(express.static('public'))
+
+
+//Configuración del directorio de las vistas hbs
+app.set('views',path.join(__dirname+'/public/views'))
+app.set('view engine','hbs')
+
+
+//Configuración del directorio que guardará los archivos partials hbs
+hbs.registerPartials(__dirname+'/public/views/partials');
+
+
+app.get('/',(req, res) => {
+    res.render('home',
+    {
+        titulo: 'home',
+        nombre: 'Diego Lopez'
+    })
+})
+
+app.get('/productos',(req, res) => {
+    res.render('productos',
+    {
+        titulo: 'Listado prodcutos',
+        nombre: 'Diego Lopez',
+        rol: 'Admin'
+    })
+})
+
+app.listen(puerto,()=>{
+    console.log(`Escuchando por el puerto ${puerto}`)
+})
